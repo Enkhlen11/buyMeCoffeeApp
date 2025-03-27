cc9b06ab903df309dbee6be5c88159b3e59629b6
24LP5569 24LP5569 <24LP5569@G304-CC0226.local>
2025-03-27 12:19:44 +0800

    back end duusaagve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
-import Image from "next/image";
+"use client";
+
 import { AddCoverImageButton } from "./_components/AddCoverImage";
 import { Button } from "@/components/ui/button";
 import { Camera } from "lucide-react";
-
+import { useEffect, useState } from "react";
 export default function Home() {
+  const [data, setData] = useState<{ data: string } | null>(null);
+  useEffect(() => {
+    fetch("./api/user")
+      .then((data) => data.json())
+      .then((json) => setData(json));
+  }, []);
+  console.log(data);
   return (
     <div>
+      {data?.data}
       <Button>
         <Camera /> Add a cover image
       </Button>
--- src/app/profile/components/CreateProfile.tsx
+++ src/app/profile/components/CreateProfile.tsx
@@ -1,17 +1,24 @@
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+import { Camera } from "lucide-react";
 
 export const CreateProfile = () => {
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <div className="w-[510px] h-[631px] border-1">
-        <p>Complete your </p>
-        <p>Add photo</p>
+    <div className="w-screen h-screen flex justify-center items-center ">
+      <div className="w-[510px] h-[631px]  flex flex-col gap-4 ">
+        <p className="text-[24px] font-bold">Complete your profile page </p>
+        <div className="flex flex-col gap-2">
+          <p>Add photo</p>
+          <div className="w-[150px] h-[150px] border-2 border-dotted rounded-full flex justify-center items-center">
+            <Camera color="gray" />
+          </div>
+        </div>
+
         <p>Name</p>
         <input
           type="text"
           placeholder="Enter your name here "
-          className="border-1 rounded-[6px]"
+          className="border-1 rounded-[6px] w-full"
         />
         <p>About</p>
         <Textarea
@@ -22,9 +29,11 @@ export const CreateProfile = () => {
         <input
           type="text"
           placeholder="https://"
-          className="border-1 rounded-[6px]"
+          className="border-1 rounded-[6px] w-full"
         />
-        <Button>Continue</Button>
+        <div className="flex justify-end">
+          <Button className="w-[246px]">Continue</Button>
+        </div>
       </div>
     </div>
   );
